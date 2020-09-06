d1ea87aab770b9d9a41cef5ba0138cf6ce7c1019
Marlon Fernandez <marlonfsolis@outlook.com>
2020-09-06 14:33:58 -0400

    Skeleton project

---
+++ src/components/user/UserDetail.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="user-detail">
+    <h3>Some User Details</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserDetail"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/user/UserEdit.vue
@@ -0,0 +1,15 @@
+<template>
+<div id="user-edit">
+  <h3>Edit the User</h3>
+</div>
+</template>
+
+<script>
+export default {
+  name: "UserEdit"
+}
+</script>
+
+<style scoped>
+
+</style>
