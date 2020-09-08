b37ef00dfb6a9dbc034cdb7c1de88d87eef031fd
Marlon Fernandez <marlonfsolis@outlook.com>
2020-09-08 03:05:20 -0400

    Using redirect for page not found.

---
+++ src/components/shared/ErrorNotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="error-not-found">
+    <h1>The page was not found</h1>
+    <p>Please check the url spelling.</p>
+  </div>
+</template>
+
+<script>
+export default {
+name: "ErrorNotFound"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/routes.js
+++ src/routes.js
@@ -4,16 +4,18 @@ import UserComplete from "@/components/user/UserComplete";
 import UserDetail from "@/components/user/UserDetail";
 import UserEdit from "@/components/user/UserEdit";
 import UserStart from "@/components/user/UserStart";
+import ErrorNotFound from "@/components/shared/ErrorNotFound";
 
 export const routes = [
-        {name: 'home', path: '', component: Home},
-        {name: 'user', path: '/user/:id', component: User},
-        {
-            name: 'user-complete', path: '/user', component: UserComplete, children: [
-                {name: 'user-start', path: '/', component: UserStart},
-                {name: 'user-detail', path: '/:id/detail', component: UserDetail},
-                {name: 'user-edit', path: '/:id/edit', component: UserEdit}
-            ]
-        }
-    ]
-;
+    {name: 'home', path: '', component: Home},
+    {name: 'user', path: '/user/:id', component: User},
+    {
+        name: 'user-complete', path: '/user', component: UserComplete, children: [
+            {name: 'user-start', path: '/', component: UserStart},
+            {name: 'user-detail', path: '/:id/detail', component: UserDetail},
+            {name: 'user-edit', path: '/:id/edit', component: UserEdit}
+        ]
+    },
+    {name: 'error-page-not-found', path: 'page-not-found', component: ErrorNotFound},
+    {path: '*', redirect: { name: 'error-page-not-found' } }
+];
